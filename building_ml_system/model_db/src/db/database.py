4a45846bdb4827cd53584e944e7363a38313e431
yunsikus <gafield8785@naver.com>
2022-01-31 21:52:14 +0900

    feat: 모델 관리 서비스 DB 설계2

--- building_ml_system/model_db/src/db/database.py
+++ building_ml_system/model_db/src/db/database.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.configurations import DBConfigurations
 
+# 출처 : https://phsun102.tistory.com/63
+# create_engine : 인자값으로 DB URL을 추가하면 DB Host에 DB 연결을 생성. 이 함수가 DB 연결의 출발점.
 engine = create_engine(
     DBConfigurations.sql_alchemy_database_url,
     encoding="utf-8",
@@ -13,19 +15,24 @@ engine = create_engine(
     echo=False,
 )
 
+# sessionmaker : 호출되었을 때, 세션을 생성해준다.
+# auticommit : api가 호출되어 DB의 내용이 변경된 경우 자동으로 commit하며 변경할지에 대한 여부를 결정한다. False로 지정한 경우에는 insert, update, delete 등으로 내용이 변경됐을 때, 수동적으로 commit을 진행해주어야 한다. 
+# autoflush : 호출되면서 commit 되지 않은 부분의 내역을 삭제할 시의 여부를 정하는 부분.
+# bind : 어떤 엔진을 통해 DB 연결을 할 지 결정하는 부분. 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base() # 상속 클래스들을 자동으로 인지하고 알아서 매핑해줌. 
+# declarative_base: 상속된 DB 모델 클래스들을 자동적으로 연결시켜주는 역할을 한다. 즉, 테이블명이 일치하는 모델을 찾아 쿼리문을 실행시켜준다. 
+Base = declarative_base() 
 
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        yield db # DB 연결 성공한 경우 DB 세션 시작
     except:
         db.rollback()
         raise
     finally:
-        db.close()
+        db.close() # DB 세션이 시작된 후, API 호출이 마무리되면 DB 세션을 닫아준다
 
 @contextmanager
 def get_context_db():
